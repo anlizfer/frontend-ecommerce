93e3f42369f103d0bd1076afe229c2440b8d20a2
Angel Lizcano <angel.lizcano.sie@gmail.com>
2023-07-17 18:39:30 -0500

    Actualización Ecommerce

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import ImgCart from './Assets/Images/shopping-cart.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [contador,setContador] = useState(0)
+
+    
+    const CambiarTexto=()=>{
+        
+        setContador(contador+1)
+    }
+
+
+    return (
+
+        <div className='navbar navbar-dark navbar-custom'>
+
+            <div className='container-fluid'>
+                <div className='row' style={{ width: "100%" }}>
+                    <div className='col-md-2'>
+                        <a className="navbar-brand" href="#">Contador {contador} veces</a>
+                    </div>
+
+                    <div className='col-md-8 text-center'>
+                        <button type="button" className="btn btn-outline-light">Explorer</button>
+                        <button type="button" className="btn btn-aparel" style={{ marginLeft: "15px" }}>Apparel</button>
+                    </div>
+
+                    <div className='col-md-2 text-end'>
+                        <button onClick={CambiarTexto} className='btn btn-light'><img className='icono-cart' src={ImgCart}></img> Cart 0</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    )
 }
 
-export default App;
+export default App
